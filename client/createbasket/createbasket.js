fdfc7c27e0e1d8bf6d24be485284cabeb1bf00df
Rhynade <rhynade_see@hotmail.com>
2016-10-23 22:29:28 +0800

    Autoform addhooks

--- client/createbasket/createbasket.js
+++ client/createbasket/createbasket.js
@@ -3,60 +3,47 @@ Template.createbasket.onCreated(function(){
 	self.subscribe('order');
 });
 
+AutoForm.addHooks(["insertOrderForm"], {
+  
+
+    onSuccess: function(operation, result, template) {
+      event.preventDefault();
+      var length = Order.find({custID: Meteor.user()._id, confirmed: false}).fetch().length;
+      var currentorder = Order.find({custID: Meteor.user()._id, confirmed: false}).fetch()[length-1]._id;
+      UserSession.set('currentorderid', currentorder);
+      FlowRouter.go('menu');
+    }
+  });
+  
+
+  Template.createbasket.events({
+    'click #add': function(){
+    event.preventDefault();
+    var orderNo = document.getElementById("order_no").value;
+    Order.update( {_id: orderNo} , { $addToSet: { custID: Meteor.user()._id }});
+    UserSession.set('currentorderid', orderNo);
+    FlowRouter.go('menu');
+    }
+  });
+
 // Template.createbasket.events({
-//   'submit form': function(e, t) {
+//   'submit form': function(event, t) {
+//     event.preventDefault();
 //   	var length = Order.find({custID: Meteor.user()._id, confirmed: false}).fetch().length;
 //   	var currentorder = Order.find({custID: Meteor.user()._id, confirmed: false}).fetch()[length-1]._id;
-//   	Session.set('currentorderid', currentorder);
+//   	UserSession.set('currentorderid', currentorder);
 //    	FlowRouter.go('menu');
 //   },
 
-//   'click #add': function(){
+//   'click #add': function(event){
+//     event.preventDefault();
 //   	var orderNo = document.getElementById("order_no").value;
-//   	Session.set('currentorderid', orderNo);
+//     Order.update( {_id: orderNo} , { $addToSet: { custID: Meteor.user()._id }});
+//   	UserSession.set('currentorderid', orderNo);
 //   	FlowRouter.go('menu');
+
 //   }
 // });
-
-// AutoForm.addHooks(["insertOrderForm"], {
-  
-
-//     onSuccess: function(operation, result, template) {
-//       var length = Order.find({custID: Meteor.user()._id, confirmed: false}).fetch().length;
-//       var currentorder = Order.find({custID: Meteor.user()._id, confirmed: false}).fetch()[length-1]._id;
-//       Session.set('currentorderid', currentorder);
-//       FlowRouter.go('menu');
-//     }
-//   });
-  
-
-  // Template.createbasket.events({
-  //   'click #add': function(){
-  //     Session.set('add', true);
-  //     var orderNo = document.getElementById("order_no").value;
-  //     Session.set('currentorderid', orderNo);
-  //     FlowRouter.go('menu');
-  //   }
-  // });
-
-Template.createbasket.events({
-  'submit form': function(event, t) {
-    event.preventDefault();
-  	var length = Order.find({custID: Meteor.user()._id, confirmed: false}).fetch().length;
-  	var currentorder = Order.find({custID: Meteor.user()._id, confirmed: false}).fetch()[length-1]._id;
-  	UserSession.set('currentorderid', currentorder);
-   	FlowRouter.go('menu');
-  },
-
-  'click #add': function(event){
-    event.preventDefault();
-  	var orderNo = document.getElementById("order_no").value;
-    Order.update( {_id: orderNo} , { $addToSet: { custID: Meteor.user()._id }});
-  	UserSession.set('currentorderid', orderNo);
-  	FlowRouter.go('menu');
-
-  }
-});
 
 Template.createbasket.helpers({
   user: () =>{
