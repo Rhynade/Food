b5db7e9e079dd3dba63e3671bf08b8bdcc923e86
jin14 <jinwei9182@gmail.com>
2016-10-25 23:02:00 +0800

    Home screen updated

--- client/homeLayout/signup.js
+++ client/homeLayout/signup.js
@@ -1,24 +1,23 @@
 Template.signup.events({
   'submit form' ( event, template ) {
     event.preventDefault();
+    // var name = event.target.name.value;
     var email = event.target.emailAddress.value;
     var password = event.target.password.value;
     let user = {
+      // roles: "Guest",
       email: email,
       password: password,
     };
     Accounts.createUser( user, ( error ) => {
-    if ( error ) {
-      Bert.alert( error.reason, 'danger' );
-    } else {
-      Bert.alert( 'Welcome!', 'success' );
-    }
     });
     event.target.emailAddress.value = "";
     event.target.password.value = "";
+    // event.target.name.value = "";
     return false;
   },
   'click #clicked': function() {
     Session.set('isClicked',false);
   }
+
 });
